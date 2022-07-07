cd55e1e0250cbeaddf4a417fba2ede32656d3342
ozhanghe <93566273+ozhanghe@users.noreply.github.com>
2022-07-06 20:02:59 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 ./ngrok config add-authtoken $NGROK
+wget https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar
+mv paper-1.16.5-794.jar paper.jar
 java -jar paper.jar
 ./ngrok tcp 25565
